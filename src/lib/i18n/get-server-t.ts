acb8f97ed236e404e0836bf55bedb29d7f7352e4
omid asghari <1381omidasghari@gmail.com>
2025-10-21 15:50:27 +0330

    end of the day

--- src/lib/i18n/get-server-t.ts
+++ src/lib/i18n/get-server-t.ts
@@ -1,9 +1,9 @@
 import { cache } from "react";
-import i18next, { type Lang } from "@/lib/i18n/i18n";
+import i18next from "@/lib/i18n/i18n";
 import { defaultNS } from "@/lib/i18n/settings";
 
 export const getServerT = cache(
-  async (lang: Lang, ns: string | string[] = defaultNS) => {
+  async (lang: "fa" | "en", ns: string | string[] = defaultNS) => {
     await i18next.changeLanguage(lang);
     const need = Array.isArray(ns) ? ns : [ns];
     await i18next.loadNamespaces(need);
